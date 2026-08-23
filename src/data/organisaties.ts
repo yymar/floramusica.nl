ec41340367d952a313411797f7ce2c000d321f11
Yoran de Haan <32639287+yymar@users.noreply.github.com>
2026-08-23 22:51:15 +0200

    Update organisaties.ts

--- src/data/organisaties.ts
+++ src/data/organisaties.ts
@@ -10,10 +10,26 @@ export interface Organisatie {
 }
 
 export const organisaties: Organisatie[] = [
-  { naam: 'Muzelinck, centrum voor de kunsten', plaats: 'Oss', url: 'https://www.muzelinck.nl/' },
-  { naam: 'MDM', plaats: 'Vught' },
-  { naam: 'Medez', plaats: 'Berlicum' },
-  { naam: 'Muziekvereniging EMM', plaats: 'Boekel' },
-  { naam: 'Muziekvereniging Zeelandia' }, // TODO: plaats bevestigen
-  { naam: 'Phoenix Cultuur' }, // TODO: plaats bevestigen
+  {
+    naam: "Muzelinck, centrum voor de kunsten",
+    plaats: "Oss",
+    url: "https://www.muzelinck.nl/",
+  },
+  { naam: "MDM", plaats: "Vught", url: "https://www.mdm.nl/" },
+  { naam: "Medez", plaats: "Berlicum", url: "https://www.medez.nl/" },
+  {
+    naam: "Muziekvereniging EMM",
+    plaats: "Boekel",
+    url: "https://www.muziekverenigingemm.nl/",
+  },
+  {
+    naam: "Muziekvereniging Zeelandia",
+    plaats: "Zeeland",
+    url: "https://muziekvereniging-zeelandia.nl/",
+  },
+  {
+    naam: "Phoenix Cultuur",
+    plaats: "Veghel",
+    url: "https://www.phoenixcultuur.nl/",
+  },
 ];
